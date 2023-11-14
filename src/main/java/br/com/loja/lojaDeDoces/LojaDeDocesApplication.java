02b2e35b07db0ac216c0e716cb4ff2e2e0ba8cba
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-11-14 14:37:17 -0300

    first commit

---
+++ src/main/java/br/com/loja/lojaDeDoces/LojaDeDocesApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja.lojaDeDoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeDocesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeDocesApplication.class, args);
+	}
+
+}
